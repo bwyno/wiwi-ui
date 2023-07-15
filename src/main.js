1363bb56563e2cd1163f584ba911d0d0265ede68
Bryan Arnoco <arnoco42@gmail.com>
2023-07-15 19:27:24 +0800

    dashboard: finished base layout

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import './assets/main.css'
+import 'primeflex/primeflex.css';
+import 'primeicons/primeicons.css';
+import "primevue/resources/themes/lara-light-indigo/theme.css";
+
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config'
+import { PrimeIcons } from 'primevue/api';
+
+
 
 import App from './App.vue'
 import router from './router'
@@ -12,5 +19,6 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(PrimeVue)
+app.use(PrimeIcons)
 
 app.mount('#app')
